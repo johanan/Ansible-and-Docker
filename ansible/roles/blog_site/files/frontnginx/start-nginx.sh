33cb34f738e658d299f7553b7d91002867f5584b
Joshua Johanan <josh359@gmail.com>
2015-04-15 23:42:59 -0400

    added swap and cadvisor

--- ansible/roles/blog_site/files/frontnginx/start-nginx.sh
+++ ansible/roles/blog_site/files/frontnginx/start-nginx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for name in VARNISH_PORT_80_TCP_ADDR
+for name in VARNISH_PORT_80_TCP_ADDR CADVISOR_PORT_8080_TCP_ADDR
 do
 	eval value=\$$name
 	sed -i "s|\${${name}}|${value}|g" /etc/nginx/conf.d/default.conf
