2cfbab58ffab7a244c94e26c28fe7c1b78061f02
Joshua Johanan <josh359@gmail.com>
2019-08-04 23:57:18 -0400

    small update to fix a few problems and add elasticsearch

--- vagrant/dev_up.sh
+++ vagrant/dev_up.sh
@@ -5,6 +5,11 @@ source ../venv/bin/activate
 vagrant up
 
 export ANSIBLE_HOST_KEY_CHECKING=False
+ansible-playbook --user=ubuntu --inventory-file=./.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory ../ansible/server.yml --skip-tags "prod"
 ansible-playbook --user=ubuntu --inventory-file=./.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory ../ansible/docker.yml --skip-tags "prod"
 
-sudo ssh -p 2222 -gNfL 80:localhost:80 ubuntu@localhost -i .vagrant/machines/default/virtualbox/private_key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
+#sudo ssh -p 2222 -gNfL 80:localhost:80 ubuntu@localhost -i .vagrant/machines/default/virtualbox/private_key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
+echo "
+rdr pass inet proto tcp from any to any port 80 -> 127.0.0.1 port 8080
+rdr pass inet proto tcp from any to any port 443 -> 127.0.0.1 port 8443
+" | sudo pfctl -ef -
